5cbc731c1bb30f5e8113262d244c13f9c0d3d0d8
showjean <showjean@hanmail.net>
2015-02-15 13:01:27 +0900

    기존 퀵스왑 단축키 (alt+gui)가 포토샵 등의 작업시 단축키와 겹쳐지는 현상이 있어, enter를 단축키에 추가. 퀵스왑 작동키는 alt+gui+enter

--- source/quickswap.c
+++ source/quickswap.c
@@ -42,28 +42,53 @@ uint8_t getQuickSwapKeyindex(uint8_t xKeyidx){
 	}
 	return xKeyidx;
 }
-static void applyQuickSwap(uint8_t xModi) {
+static void applyQuickSwap() {
 	static uint8_t prevModifier = 0;
+	static uint8_t prevKey = 0;
 
-	if(getDownBufferAt(0) > 0){
+	// 작동키 이외의 키들이 눌려 있을 경우는 취소;
+	if(getDownBufferAt(1) > 0){
 		enabledQuickSwapCount = 0;
 		return;
 	}
 	
-	if(xModi == prevModifier) return;
+	// ALT <-> GUI
+	uint8_t gModi = getModifierDownBuffer();
+	uint8_t gKey = getDownBufferAt(0);
 
-	if(xModi == 0x0C || xModi == 0xC0 || xModi == 0x48 || xModi == 0x84){	// ALT + GUI
-		_quickSwapCount = 0;
-		enabledQuickSwapCount = COUNT_TYPE_QUICKSWAP_ALT_GUI;
-	}else{		
-		enabledQuickSwapCount = 0;
+	if(gModi == 0) prevModifier = 0;
+	if(gKey == 0) prevKey = 0;
+
+	if(gModi != prevModifier && prevKey != gKey) //return;	// 같은 모디 키를 누르고 있을 때는 카운트가 초기화 되지 않도록 스킵;
+	{
+		if((gModi == 0x0C || gModi == 0xC0 || gModi == 0x48 || gModi == 0x84) && gKey == KEY_ENTER){	// ALT + GUI
+			_quickSwapCount = 0;
+			enabledQuickSwapCount = COUNT_TYPE_QUICKSWAP_ALT_GUI;
+		}else{
+			enabledQuickSwapCount = 0;
+		}
+
+		prevModifier = gModi;
+		prevKey = gKey;
 	}
 
-	prevModifier = xModi;
-
+	if (enabledQuickSwapCount && ++_quickSwapCount > quickSwapCountMax) {
+		if (enabledQuickSwapCount == COUNT_TYPE_QUICKSWAP_ALT_GUI) {
+			blinkOnce(50);
+			if ((quickSwapMode & QUICKSWAP_ALT_GUI) == QUICKSWAP_ALT_GUI) {
+				quickSwapMode &= ~QUICKSWAP_ALT_GUI;
+			} else {
+				quickSwapMode |= QUICKSWAP_ALT_GUI;
+				_delay_ms(50);
+				blinkOnce(100);
+			}
+		}
+		eeprom_write_byte((uint8_t *) EEPROM_QUICK_SWAP, quickSwapMode);
+		enabledQuickSwapCount = 0;
+	}
 }
 
-static void countQuickSwapEnabled(void){	
+/*static void countQuickSwapEnabled(void){
 	if(enabledQuickSwapCount && ++_quickSwapCount > quickSwapCountMax){
 		if(enabledQuickSwapCount == COUNT_TYPE_QUICKSWAP_ALT_GUI){
 			blinkOnce(50);
@@ -79,10 +104,10 @@ static void countQuickSwapEnabled(void){
 		enabledQuickSwapCount = 0;
 	}
 
-}
+}*/
 
 void enterFrameForQuickSwap(void){
-	applyQuickSwap(getModifierDownBuffer());
+	applyQuickSwap();
 
-	countQuickSwapEnabled();
+//	countQuickSwapEnabled();
 }
