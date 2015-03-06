222c98a8121c1fe93bfd6078fc4ddd2de4e4eca8
showjean <showjean@hanmail.net>
2015-03-07 01:46:27 +0900

    gui + alt + r shift

--- source/quickswap.c
+++ source/quickswap.c
@@ -44,51 +44,28 @@ uint8_t getQuickSwapKeyindex(uint8_t xKeyidx){
 }
 static void applyQuickSwap() {
 	static uint8_t prevModifier = 0;
-	static uint8_t prevKey = 0;
 
-	// 작동키 이외의 키들이 눌려 있을 경우는 취소;
-	if(getDownBufferAt(1) > 0){
+	if(getDownBufferAt(0) > 0){
 		enabledQuickSwapCount = 0;
 		return;
 	}
 	
-	// ALT <-> GUI
 	uint8_t gModi = getModifierDownBuffer();
-	uint8_t gKey = getDownBufferAt(0);
 
-	if(gModi == 0) prevModifier = 0;
-	if(gKey == 0) prevKey = 0;
+	if(gModi == prevModifier) return;
 
-	if(gModi != prevModifier && prevKey != gKey) //return;	// 같은 모디 키를 누르고 있을 때는 카운트가 초기화 되지 않도록 스킵;
-	{
-		if((gModi == 0x0C || gModi == 0xC0 || gModi == 0x48 || gModi == 0x84) && gKey == KEY_ENTER){	// ALT + GUI
-			_quickSwapCount = 0;
-			enabledQuickSwapCount = COUNT_TYPE_QUICKSWAP_ALT_GUI;
-		}else{
-			enabledQuickSwapCount = 0;
-		}
-
-		prevModifier = gModi;
-		prevKey = gKey;
-	}
-
-	if (enabledQuickSwapCount && ++_quickSwapCount > quickSwapCountMax) {
-		if (enabledQuickSwapCount == COUNT_TYPE_QUICKSWAP_ALT_GUI) {
-			blinkOnce(50);
-			if ((quickSwapMode & QUICKSWAP_ALT_GUI) == QUICKSWAP_ALT_GUI) {
-				quickSwapMode &= ~QUICKSWAP_ALT_GUI;
-			} else {
-				quickSwapMode |= QUICKSWAP_ALT_GUI;
-				_delay_ms(50);
-				blinkOnce(100);
-			}
-		}
-		eeprom_write_byte((uint8_t *) EEPROM_QUICK_SWAP, quickSwapMode);
+	if(gModi == 0x2C || gModi == 0xE0 || gModi == 0x68 || gModi == 0xA4){	// ALT + GUI + R SHIFT
+		_quickSwapCount = 0;
+		enabledQuickSwapCount = COUNT_TYPE_QUICKSWAP_ALT_GUI;
+	}else{		
 		enabledQuickSwapCount = 0;
 	}
-}
 
-/*static void countQuickSwapEnabled(void){
+	prevModifier = gModi;
+
+//}
+
+//static void countQuickSwapEnabled(void){
 	if(enabledQuickSwapCount && ++_quickSwapCount > quickSwapCountMax){
 		if(enabledQuickSwapCount == COUNT_TYPE_QUICKSWAP_ALT_GUI){
 			blinkOnce(50);
@@ -104,7 +81,7 @@ static void applyQuickSwap() {
 		enabledQuickSwapCount = 0;
 	}
 
-}*/
+}
 
 void enterFrameForQuickSwap(void){
 	applyQuickSwap();
